970194b84c847bdee09a4d05fb4e20b8b724d1e4
kortess <kortess1999@mail.ru>
2021-03-27 14:13:19 +0300

    Добавление БД

---
+++ database/pbdb.js
@@ -0,0 +1,15 @@
+const pgp = require('pg-promise')({
+    // Initialization Options
+});
+// Preparing the connection details:
+const cn = 'postgres://postgres:gerard789@localhost:5432/rmm_database';
+// Creating a new database instance from the connection details:
+const db = pgp(cn);
+module.exports={
+    ///Выполнение запроса для PostgreSQL
+    query: async (query)=>{
+        return await db.any(query)
+            .then(console.log(`Query ${query.substr(0,30)} executed`))
+            .catch((e)=>{console.log(`Error of query:${query.substr(0,30)}\n${e}`)});
+    },
+}
